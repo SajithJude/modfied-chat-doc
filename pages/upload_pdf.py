bcb45fab2e64900b2a74e17294401e43db61de65
Jude Sajith <judesajith.aj@gmail.com>
2023-03-25 14:26:34 -0400

    sss

--- pages/upload_pdf.py
+++ pages/upload_pdf.py
@@ -22,6 +22,20 @@ def create_index(index_file):
 
     return index
 
+def createindex(pdf_file):
+    # Load the PDF file and extract text
+    text = extract_text(pdf_file)
+
+    # Create documents
+    documents = [Document(text)]
+
+    # Create and save the index
+    filename = pdf_file.name.split(".")[0] + ".json"
+    index = GPTSimpleVectorIndex(documents)
+    index.save_to_disk(filename)
+
+    return filename
+
 # Create the app layout
 st.title("PDF Indexer")
 
@@ -29,7 +43,7 @@ pdf_file = st.file_uploader("Upload a PDF file")
 
 # If a PDF file is uploaded, create and save the index
 if pdf_file:
-    filename = create_index(pdf_file)
+    filename = createindex(pdf_file)
     st.write(filename)
     st.success(f"Index saved to {filename}")
 
